16c3273d085563914964e13b43955d7e61b6e126
lzm0010 <lzm0010@gmail.com>
2020-08-18 09:47:17 -0400

    skeleton for controllers and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+    def create
+
+    end
+
+    def index
+    
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
+  resources :comments, only: [:create, :update, :destroy]
+  resources :posts, only: [:create, :index, :show, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

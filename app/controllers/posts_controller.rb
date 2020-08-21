e91ebee779d067061b4895d4bd9ef47c8e1d9b54
lzm0010 <lzm0010@gmail.com>
2020-08-21 11:19:57 -0400

    built out post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,55 @@
 class PostsController < ApplicationController
-    def create
+    before_action :set_post, only: [:show, :update, :destroy]
 
+    def create
+        post = Post.new(post_params)
+        if @post.save
+          render json: post
+        else
+          render json: {"error": "Your post wasn't created Lee, you made an uh oh!"}
+        end
     end
+    
 
     def index
-    
+        posts = Post.all #include sort here later
+        render json: posts.to_json(:include => {
+            :comments => {:only => [:id, :user_id, :author, :content, :created_at, :updated_at]}
+        })
     end
 
     def show
-
+        render json: post
     end
 
     def update
+        if post.update(post_params)
+            render json: post.to_json(:include => {
+                :comments => {:only => [:id, :user_id, :author, :content, :created_at, :updated_at]}
+            })
 
+        else 
+            render json: {"error": "Your post wasn't updated Lee, you made an uh oh!"}
+        end
     end
 
     def destroy
-        
+        if post.destroy
+            render json: post
+        else
+            render json: {"error": "Your post wasn't deleted Lee, you made an uh oh!"}
+        end
     end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :cover_photo_url, :content, :views, :likes, :user_id)
+    end
+
+    def set_post
+        post = Post.find(params[:id])
+    end
+
+    
 end

3c81c52e3252ef348fee43727129bb2d047bf35f
lzm0010 <lzm0010@gmail.com>
2020-09-10 12:38:48 -0400

    removed email print

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,6 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    puts @contact
     @contact.request = request
     if @contact.deliver
       render json: {message: "Thank you for your Email, I will get back to you shortly!"}

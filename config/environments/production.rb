793748c24ab466be23814a19c29ae7544fdf2f1f
lzm0010 <lzm0010@gmail.com>
2020-09-24 16:48:17 -0400

    added email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => ‘yoursite.herokuapp.com’ }
-  Rails.application.routes.default_url_options[:host] = ‘yoursite.herokuapp.com’
+  config.action_mailer.default_url_options = { :host => "https://glacial-hamlet-43424.herokuapp.com" }
+  Rails.application.routes.default_url_options[:host] = "https://glacial-hamlet-43424.herokuapp.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
